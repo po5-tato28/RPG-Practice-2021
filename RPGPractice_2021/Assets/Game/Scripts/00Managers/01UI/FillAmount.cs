14e2aef9cb18652640f81ad6d4b183c0518a0e1d
po5-tato28 <c2trillions@gmail.com>
2021-09-29 03:32:21 +0900

    Update Untill Skills
    !! without motions and weapons !!

--- RPGPractice_2021/Assets/Game/Scripts/00Managers/01UI/FillAmount.cs
+++ RPGPractice_2021/Assets/Game/Scripts/00Managers/01UI/FillAmount.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FillAmount : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Button button;
+
     void Start()
     {
-        
+        button = GetComponent<Button>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickButton()
     {
-        
+        StartCoroutine(CoolTime(3f));
     }
+
+    IEnumerator CoolTime(float cool)
+    {
+        print("쿨타임 코루틴 실행");
+        button.interactable = false;
+
+        while (cool > 1.0f)
+        {
+            cool -= Time.deltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+
+        print("쿨타임 코루틴 완료");
+        button.interactable = true;
+    }
+
 }
--- RPGPractice_2021/Assets/Game/Scripts/00Managers/01UI/HealthBar.cs
+++ RPGPractice_2021/Assets/Game/Scripts/00Managers/01UI/HealthBar.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Health healthComponent = null;
+    [SerializeField] Slider slider = null;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        slider.value = healthComponent.GetHealthPoint();
     }
 }
